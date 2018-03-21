098eed2a61c9e5a2cad2aa70c74940ccee946820
DESKTOP-L7VHGES\Dino <lovac_95@hotmail.com>
2018-03-21 22:45:13 +0100

    Added all ImageViews and TextViews, implemented onClickListener
    Manifest: added         android:hardwareAccelerated="false"
                            android:largeHeap="true"
    
    due to emulator crash.

--- app/src/main/java/com/example/dino/myapp/MainActivity.java
+++ app/src/main/java/com/example/dino/myapp/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.dino.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.imgView_turing)
+    ImageView ImageView_turing;
+    @BindView(R.id.imgView_lovelace)
+    ImageView ImageView_lovelace;
+    @BindView(R.id.imgView_neumann)
+    ImageView ImageView_neumann;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        ImageView_turing.setOnClickListener(this);
+        ImageView_lovelace.setOnClickListener(this);
+        ImageView_neumann.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.imgView_turing:
+                Toast.makeText(this, R.string.String_turing_quote,Toast.LENGTH_LONG).show();
+                break;
+            case R.id.imgView_lovelace:
+                Toast.makeText(this, R.string.String_lovelace_quote,Toast.LENGTH_LONG).show();
+                break;
+            case R.id.imgView_neumann:
+                Toast.makeText(this, R.string.String_neumann_quote,Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
